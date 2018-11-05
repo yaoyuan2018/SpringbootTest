3afcea8803f77ba75901cee42f51f7f87e0913b4
yaoyuan <yy494818027@163.com>
2018-11-05 23:00:37 +0800

    运行

---
+++ src/main/java/ch2/event/Main.java
@@ -0,0 +1,13 @@
+package ch2.event;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EventConfig.class);
+        DemoPublisher demoPublisher = context.getBean(DemoPublisher.class);
+
+        demoPublisher.publish("hello application event");
+        context.close();
+    }
+}
